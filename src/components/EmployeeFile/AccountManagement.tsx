b1be8eb02a685b4e5a0a9ed13aac62589688b6cc
Dominik <dominik.merdzik@onet.pl>
2024-02-06 13:31:31 +0100

    User is unable to change the password if he provides invalid one or if the old and new one is equal, also provided email address must be compliant

--- src/components/EmployeeFile/AccountManagement.tsx
+++ src/components/EmployeeFile/AccountManagement.tsx
@@ -4,32 +4,127 @@ import InteractiveBackground from '../Utils/InteractiveBackground'
 import Button from '../Utils/Button'
 import classes from './AccountManagement.module.scss'
 
+const isPasswordValid = (password: string) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
 const AccountManagement: React.FC = () => {
+    const [email, setEmail] = useState<string>('')
+    const [emailError, setEmailError] = useState<string>('')
     const [oldPassword, setOldPassword] = useState<string>('')
     const [newPassword, setNewPassword] = useState<string>('')
-    const token = localStorage.getItem('authToken') || '';
+    const [passwordError, setPasswordError] = useState<string>('')
+    const [passwordChanged, setPasswordChanged] = useState<boolean>(false)
+    const [loggedInUserEmail, setLoggedInUserEmail] = useState<string>('');
 
-    const handlePasswordChange = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:8000/api/password_change', {
-                method: 'POST',
+    const token = localStorage.getItem('authToken') || ''
+
+    useEffect(() => {
+        const fetchLoggedInUserEmail = async () => {
+          try {
+            if (token) {
+              const response = await fetch('http://127.0.0.1:8000/api/user', {
+                method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Token ${token}`,
+                  'Authorization': `Token ${token}`,
                 },
-                body: JSON.stringify({
-                    old_password: oldPassword,
-                    new_password: newPassword
-                }),
-            });
-
-            if (response.ok) {
-                console.log('Hasło zostało pomyślnie zmienione.');
-            } else {
-                console.error('Błąd podczas zmiany hasła.');
+              });
+    
+              if (!response.ok) {
+                console.log('Błąd podczas pobierania e-maila.');
+                return;
+              }
+    
+              const data = await response.json();
+              const userEmail = data.email;
+              setLoggedInUserEmail(userEmail);
             }
+          } catch (error) {
+            console.error('Błąd logowania:', error instanceof Error ? error.message : 'Nieznany błąd');
+          }
+        };
+    
+        fetchLoggedInUserEmail();
+    }, [token]);
+
+    const handleCheckEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+
+        if (loggedInUserEmail !== e.target.value) {
+            setEmailError('Podany adres e-mail nie jest poprawny.')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOldPassword(e.target.value)
+        setPasswordError('')
+        setPasswordChanged(false)
+    }
+
+    const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewPassword(e.target.value)
+        setPasswordError('')
+        setPasswordChanged(false)
+    }
+
+    const handleClearForm = () => {
+        setOldPassword('')
+        setNewPassword('')
+        setPasswordChanged(false)
+        setPasswordError('')
+        setEmail('')
+    }
+    
+    const handleSubmit = async () => {
+        try {
+          if (token) {
+            if (oldPassword === newPassword) {
+              setPasswordError('Hasła nie mogą być takie same.');
+              return;
+            }
+    
+            if (!isPasswordValid(newPassword)) {
+              setPasswordError('Nowe hasło nie spełnia wymagań złożoności.');
+              return;
+            }
+    
+            if (loggedInUserEmail !== email) {
+              setEmailError('Podany adres e-mail nie jest poprawny.');
+              return;
+            }
+    
+            const response = await fetch('http://127.0.0.1:8000/api/password_change', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`,
+              },
+              body: JSON.stringify({
+                old_password: oldPassword,
+                new_password: newPassword,
+              }),
+            });
+    
+            if (response.ok) {
+              console.log('Hasło zostało pomyślnie zmienione.');
+              setPasswordChanged(true);
+            } else if (response.status === 400) {
+                const res = await response.json()
+                const { detail } = res
+
+                console.log(`Błąd podczas zmiany hasła: ${detail}`)
+
+                setPasswordError(detail)
+            } else {
+              console.log('Błąd podczas zmiany hasła.');
+              setPasswordChanged(false);
+            }
+          }
         } catch (error) {
-            console.error(`Wystąpił błąd: ${error}`);
+          console.log(`Wystąpił błąd: ${error}`);
         }
     };
 
@@ -43,8 +138,15 @@ const AccountManagement: React.FC = () => {
                 <div className={classes['accountManagement__data_container']}>
                     <div className={classes['accountManagement__field_container']}>
                         <label>Login </label>
-                        <input type="text" className={classes['accountManagement__input']} placeholder="Wprowadź adres email"></input>
+                        <input 
+                            type="text" 
+                            className={classes['accountManagement__input']} 
+                            placeholder="Wprowadź adres email" 
+                            value={email}
+                            onChange={handleCheckEmail}
+                        />
                     </div>
+                    {emailError && <p className={classes['accountManagement__message-error']}>{emailError}</p>}
                     <div className={classes['accountManagement__field_container']}>
                         <label>Stare hasło </label>
                         <input
@@ -52,27 +154,29 @@ const AccountManagement: React.FC = () => {
                             className={classes['accountManagement__input']} 
                             placeholder="Wprowadź stare hasło"
                             value={oldPassword}
-                            onChange={(event) => setOldPassword(event.target.value)}
+                            onChange={handlePasswordChange}
                         />
                     </div>
                     <div className={classes['accountManagement__field_container']}>
                         <label>Nowe hasło </label>
-                        <input 
-                            type="password" 
-                            className={classes['accountManagement__input']} 
+                        <input
+                            type="password"
+                            className={classes['accountManagement__input']}
                             placeholder="Wprowadź nowe hasło"
                             value={newPassword}
-                            onChange={(event) => setNewPassword(event.target.value)}
+                            onChange={handleConfirmPasswordChange}
                         />
                     </div>
+                    {passwordError && <p className={classes['accountManagement__message-error']}>{passwordError}</p>}
+                    {passwordChanged && <p className={classes['accountManagement__message-success']}>Hasło zostało pomyślnie zmienione!</p>}
                     <div className={classes['accountManagement__field_container']}>
                         <label>Weryfikacja hasła </label>
                         <input type="text" className={classes['accountManagement__input']} placeholder="weryfikacja hasła" disabled></input>
                     </div>
                 </div>
                 <div className={classes['accountManagement__button_container']}>
-                    <Button type="submit" onClick={handlePasswordChange} text="Zapisz"/>
-                    <Button type="submit" text="Anuluj"/>
+                    <Button type="submit" onClick={handleSubmit} text="Zapisz"/>
+                    <Button type="submit" onClick ={handleClearForm} text="Anuluj"/>
                 </div>
             </section>
             <InteractiveBackground />
